78730e913896e9e5df020e135ce1ed49e74f6e16
Eduardo <esep@uc.cl>
2025-12-07 17:01:50 -0300

    fix base path

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GiftsPage from './pages/GiftsPage/GiftsPage';
 import PurchasePage from './pages/PurchasePage';
+import { BASE_PATH } from '../PARAMS.json';
 
 function Router() {
   return (
     <Routes>
-      <Route path='/' element={ <GiftsPage /> } />
-      <Route path='/regalos' element={ <GiftsPage /> } />
-      <Route path='/pago' element={ <PurchasePage /> } />
+      <Route path={ `${BASE_PATH}/` } element={ <GiftsPage /> } />
+      <Route path={ `${BASE_PATH}/regalos` } element={ <GiftsPage /> } />
+      <Route path={ `${BASE_PATH}/pago` } element={ <PurchasePage /> } />
     </Routes>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { BASE_PATH } from './PARAMS.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MarriagePageFront/",
+  base: BASE_PATH,
   server: {
     watch: {
       usePolling: true
