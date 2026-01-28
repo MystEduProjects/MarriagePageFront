410449cfa4e602d2deefa9aa22ba2f550e58e5b1
Eduardo <esep@uc.cl>
2026-01-27 23:06:57 -0300

    fix path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { BASE_PATH } from './PARAMS.json';
+import { BASE_PATH } from '../PARAMS.json';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
