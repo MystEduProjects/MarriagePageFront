fcebd96dca6094cba7efc1c3472150ad6c468174
Eduardo <esep@uc.cl>
2026-01-31 16:15:12 -0300

    add maps

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -37,13 +37,22 @@ const MainPage = () => {
         <div className="space-y-12">
           <h2 className="text-4xl italic mb-8 border-b border-[#eeeae3] pb-4">Dónde</h2>
           
+          {/* Ceremonia */}
           <div className="group space-y-4">
             <h3 className="text-[10px] uppercase tracking-[0.2em] text-[#a0a0a0]">Ceremonia</h3>
             <p className="text-xl">Iglesia Providencia</p>
-            <p className="text-sm font-sans text-[#8c8c8c]">Insertar Dirección, 123</p>
-            <div className="h-48 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition-all duration-700 shadow-sm">
-              {/* Aquí iría el iframe de Google Maps */}
-              <div className="bg-[#eeeae3] w-full h-full flex items-center justify-center">Mapa Ceremonia</div>
+            <p className="text-sm font-sans text-[#8c8c8c]">Insertar Direccion, 123</p>
+            <div className="h-56 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition-all duration-700 shadow-md border border-[#eeeae3]">
+              <iframe
+                title="Mapa Ceremonia"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.0089288588733!2d-70.57701261701051!3d-33.44907390477822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662ce4c9c133db3%3A0xd1a686bd84810a7b!2sAv.%20Ossa%20528%2C%207790534%20%C3%91u%C3%B1oa%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1sen!2scl!4v1769886574316!5m2!1sen!2scl"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
 
@@ -51,9 +60,18 @@ const MainPage = () => {
             <h3 className="text-[10px] uppercase tracking-[0.2em] text-[#a0a0a0]">Cena & Fiesta</h3>
             <p className="text-xl">Hotel DoubleTree by Hilton, Piso 18</p>
             <p className="text-sm font-sans text-[#8c8c8c]">Av Vitacura 2727, Las Condes</p>
-            <div className="h-48 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition-all duration-700 shadow-sm">
-              <div className="bg-[#eeeae3] w-full h-full flex items-center justify-center">Mapa Fiesta</div>
-            </div>
+            <div className="h-56 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition-all duration-700 shadow-md border border-[#eeeae3]">
+            <iframe
+              title="Mapa Fiesta"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.2862210472317!2d-70.60879101702041!3d-33.41578150482549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf421eaa4e59%3A0x877d7f9a911c33aa!2sDoubleTree%20by%20Hilton%20Hotel%20Santiago%20-%20Vitacura!5e0!3m2!1sen!2scl!4v1769886841938!5m2!1sen!2scl"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
           </div>
         </div>
 
